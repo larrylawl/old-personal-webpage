46760e94e6c81a8d5bd227edcb8b67396477cf48
Larry Law <larrylaw97@gmail.com>
2020-06-08 16:19:01 +0800

    Add KS stats post

--- script/convert.sh
+++ script/convert.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
+#### HOW TO USE ####
+# `bash script/convert.sh %filepath`
+# This translates to 
+# `convert %filepath -resize 1024x600\! %filepath`
+####
 
-IMAGE=$1
+FILEPATH=$1
 DEFAULT_WIDTH=1024
 DEFAULT_HEIGHT=600
 
-cd $FILEPATH
-convert $IMAGE -resize ${DEFAULT_WIDTH}x${DEFAULT_HEIGHT}\! $IMAGE
+# echo "the PWD is : ${pwd}"
+convert $FILEPATH -resize ${DEFAULT_WIDTH}x${DEFAULT_HEIGHT}\! $FILEPATH
 
